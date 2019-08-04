1e8ae1d927d3ecc64fb1274479e1f34e1e148e47
DzinnApp <dzinnapp@gmail.com>
2019-08-04 05:40:49 +0530

    Initia commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 80;
+
+
+
+//path is used to go back a directory neatly by __dirname , '../public'
+const path = require('path');
+const publicPath = path.join(__dirname, './public'); 
+
+//setup the server to serve static files from the public folder
+app.use(express.static(publicPath));
+
+app.get('/hikush',(req,res)=>{
+    res.send("hello Kush");
+})
+app.get('/', function (req, res) {
+    res.sendFile('index.html');
+});
+app.get('/resume_new.pdf', function (req, res) {
+    res.sendFile('Resume_new.pdf');
+});
+app.get('/resume_new.docx', function (req, res) {
+    res.sendFile('Resume_new.pdf');
+});
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});
---
+++ public/sw.js
@@ -0,0 +1,40 @@
+const cacheName = 'guglani-tech';
+const filesToCache = [
+  '/',
+  '/assets/css/app.css',
+  '/assets/media/bg2.svg',
+  '/assets/media/nodejs.svg',
+  '/assets/media/education.svg',
+  '/assets/media/firebase.svg',
+  '/assets/js/AboutSection.js',
+  '/assets/js/EducationSection.js',
+  '/assets/js/ExperienceSection.js',
+  '/assets/js/MainApp.js',
+  '/assets/js/npm.fbjs.js',
+  '/assets/js/ProjectItem.js',
+  '/assets/js/ProjectSection.js',
+  '/assets/js/SkillCard.js',
+  '/assets/js/SkillCardSection.js',
+  '/assets/js/Timeline.js',
+];
+self.addEventListener('install', (e) => {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName)
+      .then((cache) => {
+        console.log('[ServiceWorker] Caching app shell');
+        return cache.addAll(filesToCache);
+      })
+      .catch(function (e) {
+        console.log("SW Caching Error", e)
+      }),
+  );
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request, { ignoreSearch: true }).then(response => response || fetch(event.request)),
+  );
+});
