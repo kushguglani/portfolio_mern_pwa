e9597122320589694d8166517cd65fae432a783a
DzinnApp <dzinnapp@gmail.com>
2019-08-04 06:07:33 +0530

    Download resume

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 const port = process.env.PORT || 80;
 
 
-
 //path is used to go back a directory neatly by __dirname , '../public'
 const path = require('path');
 const publicPath = path.join(__dirname, './public'); 
@@ -17,9 +16,9 @@ app.get('/hikush',(req,res)=>{
 app.get('/', function (req, res) {
     res.sendFile('index.html');
 });
-app.get('/resume_new.pdf', function (req, res) {
-    res.sendFile('Resume_new.pdf');
-});
+// app.get('/resume_new.pdf', function (req, res) {
+//     res.sendFile('Resume_new.pdf');
+// });
 app.get('/resume_new.docx', function (req, res) {
     res.sendFile('Resume_new.pdf');
 });
