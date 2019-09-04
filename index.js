c8e6a384a088d09d25a0c9d94a677539df8d6e1f
DzinnApp <dzinnapp@gmail.com>
2019-09-04 16:33:18 +0530

    index.js

--- index.js
+++ index.js
@@ -5,22 +5,24 @@ const port = process.env.PORT || 80;
 
 //path is used to go back a directory neatly by __dirname , '../public'
 const path = require('path');
-const publicPath = path.join(__dirname, './public'); 
+const publicPath = path.join(__dirname, './public');
 
 //setup the server to serve static files from the public folder
 app.use(express.static(publicPath));
 
-app.get('/hikush',(req,res)=>{
+app.get('/hikush', (req, res) => {
     res.send("hello Kush");
 })
 app.get('/', function (req, res) {
     res.sendFile('index.html');
 });
 app.get('/resume_new', function (req, res) {
-    res.sendFile('Resume_new.pdf');
+    const file = `${__dirname}/public/Resume_new.pdf`;
+    res.download(file);
 });
 app.get('/resume_docx', function (req, res) {
-    res.sendFile('Resume_new.docx');
+    const file = `${__dirname}/public/Resume_new.docx`;
+    res.download(file);
 });
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
